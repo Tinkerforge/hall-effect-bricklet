5279e918aa42f2154608e8a132500d56866f225e
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-01-10 15:48:14 +0100

    Add Perl examples

---
+++ software/examples/perl/example_callback.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletHallEffect;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => '7xwQ9g'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $he = BrickletHallEffect->new(&UID, $ipcon); # Create device object
+
+# Callback function for edge count callback
+sub cb_edge_count
+{
+    my ($edge_count, $value) = @_;
+    print "\nEdge Count: $edge_count\n";
+}
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Set Period for edge_count callback to 0.05s (50ms)
+# Note: The edge_count callback is only called every 50ms if the 
+#       edge_count has changed since the last call!
+$he->set_edge_count_callback_period(50);
+
+# Register edge count callback to function cb_edge_count
+$he->register_callback($he->CALLBACK_EDGE_COUNT, 'cb_edge_count');
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
+
---
+++ software/examples/perl/example_simple.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletHallEffect;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => '7xwQ9g'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $he = BrickletHallEffect->new(&UID, $ipcon); # Create device object
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Get current edge count of encoder without reset 
+my $edge_count = $he->get_edge_count(0);
+
+print "\nEdgeCount: $edge_count\n";
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
+
