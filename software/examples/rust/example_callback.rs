01c57a075b4eefe5cc4c4f425a60627f1e5d7a1f
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:21:03 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,31 +1,32 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{hall_effect_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{hall_effect_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Hall Effect Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Hall Effect Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let hall_effect_bricklet = HallEffectBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let he = HallEffectBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for edge count events.
-    let edge_count_listener = hall_effect_bricklet.get_edge_count_receiver();
-    // Spawn thread to handle received events. This thread ends when the hall_effect_bricklet
+    // Create receiver for edge count events.
+    let edge_count_receiver = he.get_edge_count_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `he` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in edge_count_listener {
-            println!("Count: {}", event.count);
+        for edge_count in edge_count_receiver {
+            println!("Count: {}", edge_count.count);
         }
     });
 
-    // Set period for edge count listener to 0.05s (50ms)
+    // Set period for edge count receiver to 0.05s (50ms).
     // Note: The edge count callback is only called every 0.05 seconds
     //       if the edge count has changed since the last call!
-    hall_effect_bricklet.set_edge_count_callback_period(50);
+    he.set_edge_count_callback_period(50);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,20 +1,20 @@
 use std::{error::Error, io};
 
-use tinkerforge::{hall_effect_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{hall_effect_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Hall Effect Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Hall Effect Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let hall_effect_bricklet = HallEffectBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let he = HallEffectBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current edge count without reset
-    let count = hall_effect_bricklet.get_edge_count(false).recv()?;
+    // Get current edge count without reset.
+    let count = he.get_edge_count(false).recv()?;
     println!("Count: {}", count);
 
     println!("Press enter to exit.");
